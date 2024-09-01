ec5c9b8944d61a199cbc8f49900a9e758f185e2d
devosc <devosc11@outlook.com>
2024-09-01 12:17:03 +0200

    Creacion Cabecera

---
+++ R1_buscadorVideos/src/Header.jsx
@@ -0,0 +1,7 @@
+export function Cabecera() {
+    return (
+    <div className="cabecera">
+        <p className="title">BUSCADOR VÍDEOS YOUTUBE</p>
+    </div>
+    );
+}
--- R1_buscadorVideos/vite.config.js
+++ R1_buscadorVideos/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true, // Esta línea fuerza a Vite a revisar cambios de archivos de manera efectiva en WSL
+    },
+    host: true, // Esto permite que Vite use la red local
+    port: 5177,
+   }, // Puedes cambiar el puerto si es necesario
 })
